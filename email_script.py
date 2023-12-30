74f1cc97b2586ee8b6513c5cba2b3c29ef208b1d
Kianoush Haratiannejadi <haratiank2@gmail.com>
2023-12-30 10:23:02 -0500

    code

---
+++ email_script.py
@@ -0,0 +1,26 @@
+
+"""
+@author: Kianoush 
+
+GitHUb: https://github.com/Kianoush-h
+YouTube: https://www.youtube.com/channel/UCvf9_53f6n3YjNEA4NxAkJA
+LinkedIn: https://www.linkedin.com/in/kianoush-haratiannejadi/
+
+Email: haratiank2@gmail.com
+
+"""
+
+import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import os
+import time
+
+
+# Email configuration
+
+sender_email = "XXXXX"  # Replace with your email
+password = "XXXXXX"  # Replace with your password
+
