dc473458ce5aed9a5a104e7b35ca9c6374cd11af
Kianoush Haratiannejadi <haratiank2@gmail.com>
2023-12-30 10:23:30 -0500

    code with test images

--- email_script.py
+++ email_script.py
@@ -24,3 +24,24 @@ import time
 sender_email = "XXXXX"  # Replace with your email
 password = "XXXXXX"  # Replace with your password
 
+
+
+subject = "90% OFF NOW | CHECK US PLEASE :)"
+
+# Read or create "sended.csv" file
+sended_file_path = "sended.csv"
+if os.path.exists(sended_file_path):
+    sended_data = pd.read_csv(sended_file_path)
+else:
+    sended_data = pd.DataFrame(columns=['email'])
+    
+
+data = pd.read_csv("../6-3385.csv")
+
+image_path = "test/test.webp"  # Replace with the actual path to your image file
+image_path2 = "test/test2.jpg"  # Replace with the actual path to your image file
+image_path3 = "test/test3.jpg"  # Replace with the actual path to your image file
+image_path4 = "test/test4.jpg"  # Replace with the actual path to your image file
+image_path5 = "test/test5.jpg"  # Replace with the actual path to your image file
+
+
